d446e7b71fe399d15b81f2e827e46d893cf4f736
NinosMorad <149583259+NinosMorad@users.noreply.github.com>
2026-08-24 22:15:37 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -8,8 +8,8 @@ import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
-    <section className="h-full">
-      <div className="container mx-auto h-full">
+    <section className="min-h-full">
+      <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
